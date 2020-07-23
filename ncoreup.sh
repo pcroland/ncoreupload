0d0f01c35267cb680531536b41cb0cc22e414cce
pcroland <pcroland@protonmail.ch>
2020-07-23 18:55:27 +0200

    fix order

--- ncoreup.sh
+++ ncoreup.sh
@@ -33,14 +33,6 @@ print_separator() {
   printf '%.0s─' $(seq 1 "$COLUMNS")
 }
 
-# Config.
-# shellcheck disable=SC1090
-source "$script_path"/ncoreupload.conf
-[[ -z "$torrent_program" ]] && torrent_program='mktor'
-[[ -z "$generate_images" ]] && generate_images='true'
-[[ -z "$print_infobar" ]] && print_infobar='false'
-[[ -z "$anonymous_upload" ]] && anonymous_upload='false'
-
 # Anonymous upload config.
 if [[ ${anonymous_upload:?} == true ]]; then
   anonymous='igen'
@@ -63,6 +55,14 @@ if [ ! -f "$cookies" ]; then
   curl 'https://ncore.cc/login.php' -c "$cookies" -s -d "submitted=1" --data-urlencode "nev=$username" --data-urlencode "pass=$password" -d "ne_leptessen_ki=1"
   print_separator
 fi
+
+# Config setup.
+# shellcheck disable=SC1090
+source "$script_path"/ncoreupload.conf
+[[ -z "$torrent_program" ]] && torrent_program='mktor'
+[[ -z "$generate_images" ]] && generate_images='true'
+[[ -z "$print_infobar" ]] && print_infobar='false'
+[[ -z "$anonymous_upload" ]] && anonymous_upload='false'
 
 # Grabbing the getUnique id.
 printf "Grabbing getUnique id: "
