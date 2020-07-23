1253d7d0d15161380e4749a2fe5f9991357b41b4
pcroland <pcroland@protonmail.ch>
2020-07-23 22:36:19 +0200

    config downloader fix

--- ncoreup.sh
+++ ncoreup.sh
@@ -37,7 +37,7 @@ cookies=~/.ncoreupload/cookies.txt
 config=~/.ncoreupload/ncoreupload.conf
 if [[ ! -f "$config" ]]; then
   printf 'Creating config file in: \e[93m%s\e[0m\n' "$config"
-  curl https://raw.githubusercontent.com/pcroland/ncoreupload/master/ncoreupload.conf -o "$config" -s
+  curl "https://raw.githubusercontent.com/pcroland/ncoreupload/master/ncoreupload.conf --create-dirs" -o "$config" -s
 fi
 
 # Searching for ncore_cookies.txt next to the script,
